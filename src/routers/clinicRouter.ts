6da4ddb5308a8be13d4fdcda1cd38fa08e476c2d
Ray Chan <ray25222@gmail.com>
2021-02-28 00:31:13 +0800

    Register clinic

--- src/routers/clinicRouter.ts
+++ src/routers/clinicRouter.ts
@@ -1,9 +1,41 @@
 import express from 'express';
+import Joi from 'joi';
+
+import { Clinic } from '../db/models';
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-  res.send('/register');
+const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  name: Joi.string().required(),
+  phoneNumber: Joi.string().required(),
+  address: Joi.string().required(),
+});
+
+router.post('/register', async (req, res) => {
+  try {
+    const body = await registerSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+
+    const existingClinic = await Clinic.findOne({
+      where: { email: body.email },
+    });
+
+    if (existingClinic) {
+      throw new Error('clinic exists');
+    }
+
+    const result = await Clinic.create(body);
+
+    res.send(result);
+  } catch (err) {
+    res.status(400).send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 router.post('/login', (req, res) => {
