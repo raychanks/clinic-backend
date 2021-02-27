3ee2e5534652227ccbe1788cc18350276bcfc3da
Ray Chan <ray25222@gmail.com>
2021-02-28 00:52:28 +0800

    Handle login

--- src/db/models/Clinic.ts
+++ src/db/models/Clinic.ts
@@ -2,7 +2,14 @@ import { Model, DataTypes } from 'sequelize';
 
 import sequelize from '../sequelize';
 
-class Clinic extends Model {}
+class Clinic extends Model {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+  public name!: string;
+  public phoneNumber!: string;
+  public address!: string;
+}
 
 Clinic.init(
   {
--- src/routers/clinicRouter.ts
+++ src/routers/clinicRouter.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import Joi from 'joi';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
 
 import { Clinic } from '../db/models';
+
+const tokenSecret: string = config.get('tokenSecret');
 
 const router = express.Router();
 
@@ -14,27 +18,31 @@ const registerSchema = Joi.object({
   address: Joi.string().required(),
 });
 
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
 router.post('/register', async (req, res) => {
   try {
     const body = await registerSchema.validateAsync(req.body, {
       abortEarly: false,
     });
-
     const existingClinic = await Clinic.findOne({
       where: { email: body.email },
     });
 
     if (existingClinic) {
-      throw new Error('clinic exists');
+      throw new Error('clinic_exists');
     }
 
     const hashedPassword = await bcrypt.hash(body.password, 10);
-    const result = await Clinic.create({
+
+    await Clinic.create({
       ...body,
       password: hashedPassword,
     });
-
-    res.send(result);
+    res.send({ success: true });
   } catch (err) {
     res.status(400).send({
       success: false,
@@ -43,8 +51,50 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', (req, res) => {
-  res.send('/login');
+router.post('/login', async (req, res) => {
+  try {
+    const body = await loginSchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
+    const existingClinic = await Clinic.findOne({
+      where: { email: body.email },
+    });
+
+    if (!existingClinic) {
+      res.status(404).send({
+        success: false,
+        message: 'clinic_not_found',
+      });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      body.password,
+      existingClinic.password,
+    );
+
+    if (!isPasswordValid) {
+      res.status(401).send({
+        success: false,
+        message: 'invalid_credentials',
+      });
+      return;
+    }
+
+    const token = jwt.sign({ id: existingClinic.id }, tokenSecret, {
+      expiresIn: '1d',
+    });
+
+    res.send({
+      success: true,
+      token,
+    });
+  } catch (err) {
+    res.status(400).send({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 export default router;
